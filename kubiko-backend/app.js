a55994ba21828ad6cf4a21a77cd47cb78cc82a17
Aayush Samaiyar <68498029+aayushsamaiyar@users.noreply.github.com>
2021-03-23 00:13:31 +0530

    Add files via upload

---
+++ kubiko-backend/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express()
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+const cookieParser = require('cookie-parser');
+
+const auth = require('./routes/auth');
+const student = require('./routes/student');
+const sensei = require('./routes/sensei');
+const passport = require('passport');
+const passportConfig = require('./routes/passport')
+const isSensei = require('./isSensei')
+const isStudent = require('./isStudent')
+
+mongoose.connect(`mongodb://localhost/QuizHack`, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, () => {
+    console.log('Successfully connected to Database!!');
+});
+
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(cookieParser())
+
+app.use('/user', auth)
+app.use('/students', passport.authenticate('jwt', { session: false }), isStudent, student)
+app.use('/teachers', passport.authenticate('jwt', { session: false }), isSensei, sensei)
+
+
+const port = process.env.PORT || 8000
+
+app.get('/', (req, res) => {
+    res.status(200).json({ msg: 'Working' })
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port} 🚀`)
+})
